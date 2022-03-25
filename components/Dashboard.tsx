da08be5e957b0fb629d06636729515de70443fd3
Amer K <26983729+malcodeman@users.noreply.github.com>
2022-03-25 20:40:51 +0100

    Adds collections

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -7,19 +7,57 @@ import {
   MenuButton,
   MenuItem,
   MenuList,
+  Text,
 } from "@chakra-ui/react";
 import { ChevronDown, LogOut, Settings } from "react-feather";
+import { map } from "ramda";
 
 import { supabase } from "../utils/supabaseClient";
 import { useSession } from "../hooks/useSession";
 
 import Account from "./Account";
 
+type Collection = {
+  id: number;
+  name: string;
+};
+
 const Dashboard = () => {
   const session = useSession();
+  const [collections, setCollections] = React.useState<Collection[]>([]);
+
+  React.useEffect(() => {
+    getCollections();
+  }, [session]);
+
+  const getCollections = async () => {
+    try {
+      const user = supabase.auth.user();
+      const { data, error, status } = await supabase
+        .from("collections")
+        .select(`id, name`)
+        .eq("user_id", user?.id);
+
+      if (error && status !== 406) {
+        throw error;
+      }
+      if (data) {
+        setCollections(data);
+      }
+    } catch (error) {}
+  };
 
   const handleSignOut = async () => {
     await supabase.auth.signOut();
+  };
+
+  const handleAddCollection = async () => {
+    const user = supabase.auth.user();
+    const { data, error } = await supabase
+      .from("collections")
+      .insert([{ user_id: user?.id, name: "Untitled" }], {
+        returning: "minimal",
+      });
   };
 
   return (
@@ -41,6 +79,14 @@ const Dashboard = () => {
             </MenuItem>
           </MenuList>
         </Menu>
+        <Text paddingX="2">Collection</Text>
+        {map(
+          (item) => (
+            <Text paddingX="2">{item.name}</Text>
+          ),
+          collections
+        )}
+        <Button onClick={handleAddCollection}>Add collection</Button>
       </Box>
       <Box>
         Bookmarks
