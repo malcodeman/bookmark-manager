27e79f0259abb177265d62de24b8379c01fef962
Amer K <26983729+malcodeman@users.noreply.github.com>
2022-04-06 23:13:17 +0200

    Adds dashboard cta

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,5 +1,35 @@
+import { Button, Center, Text, useToast } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+
+import useCollections from "../data/useCollections";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { insertCollection } = useCollections();
+  const router = useRouter();
+  const toast = useToast();
+
+  const handleAddCollection = async () => {
+    const resp = await insertCollection();
+    if (resp.error) {
+      toast({
+        title: `${resp.error.message}`,
+        status: "error",
+        isClosable: true,
+      });
+    }
+    if (resp.data) {
+      router.push(`/${resp.data.id}`);
+    }
+  };
+
+  return (
+    <Center minHeight="100vh" flexDirection="column">
+      <Text mb="2">Select a collection or create a new one</Text>
+      <Button size="sm" onClick={handleAddCollection}>
+        Create a new collection
+      </Button>
+    </Center>
+  );
 };
 
 export default Dashboard;
